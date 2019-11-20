86214f4357df2f307fdc3a80682244d89b7173ed
OntheDOL <OntheDOL@users.noreply.github.com>
2019-11-20 13:23:58 +0800

    0x70 packet update for 1125
    Temporary fix for group window on 1125d. Map dot still not working. Still need to put in some callback to check and send group members map location updates.

--- GameServer/packets/Server/PacketLib1125.cs
+++ GameServer/packets/Server/PacketLib1125.cs
@@ -382,6 +382,135 @@ namespace DOL.GS.PacketHandler
             }
         }
 
+		
+		/// <summary>
+        /// updated group window packet for 1125
+        /// </summary>
+        public override void SendGroupWindowUpdate()
+        {
+            if (GameClient.Player == null) return;
+
+            using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.VariousUpdate)))
+            {
+                pak.WriteByte(0x06); // subcode - player group window
+				// a 06 00 packet is sent when logging in.
+                Group group = GameClient.Player.Group;
+                if (group == null)
+                {
+                    pak.WriteByte(0x00); // a 06 00 packet is sent when logging in.						
+                }
+                else
+                {
+                    pak.WriteByte((byte)group.MemberCount);
+					foreach (GameLiving living in group.GetMembersInTheGroup())
+                    {                        
+                        pak.WritePascalString(living.Name);
+                        pak.WritePascalString(living is GamePlayer ? ((GamePlayer)living).CharacterClass.Name : "NPC");
+                        pak.WriteShort((ushort)living.ObjectID); //or session id?
+                        pak.WriteByte(living.Level);
+                    }   
+                }
+                
+                SendTCP(pak);
+            }
+        }
+			
+
+		protected override void WriteGroupMemberUpdate(GSTCPPacketOut pak, bool updateIcons, GameLiving living)
+        {
+            pak.WriteByte((byte)(0x20 | living.GroupIndex)); // From 1 to 8 // 0x20 is player status code
+            bool sameRegion = living.CurrentRegion == GameClient.Player.CurrentRegion;
+            GamePlayer player = null;
+
+            if (sameRegion)
+            {
+                player = living as GamePlayer;
+
+                if (player != null)
+                {
+                    pak.WriteByte(player.CharacterClass.HealthPercentGroupWindow);
+                }
+                else
+                {
+                    pak.WriteByte(living.HealthPercent);
+                }
+                pak.WriteByte(living.ManaPercent);
+                pak.WriteByte(living.EndurancePercent); // new in 1.69
+
+                byte playerStatus = 0;
+                if (!living.IsAlive)
+                {
+                    playerStatus |= 0x01;
+                }
+                if (living.IsMezzed)
+                {
+                    playerStatus |= 0x02;
+                }
+                if (living.IsDiseased)
+                {
+                    playerStatus |= 0x04;
+                }
+                if (SpellHelper.FindEffectOnTarget(living, "DamageOverTime") != null)
+                {
+                    playerStatus |= 0x08;
+                }
+                if (living is GamePlayer)
+                {
+                    if ((living as GamePlayer).Client.ClientState == GameClient.eClientState.Linkdead)
+                    {
+                        playerStatus |= 0x10;
+                    }
+                }
+                if (!sameRegion)
+                {
+                    playerStatus |= 0x20;
+                }
+                if (living.DebuffCategory[(int)eProperty.SpellRange] != 0 || living.DebuffCategory[(int)eProperty.ArcheryRange] != 0)
+                {
+                    playerStatus |= 0x40;
+                }
+                pak.WriteByte(playerStatus);
+                // 0x00 = Normal , 0x01 = Dead , 0x02 = Mezzed , 0x04 = Diseased ,
+                // 0x08 = Poisoned , 0x10 = Link Dead , 0x20 = In Another Region, 0x40 - NS
+
+                WriteGroupMemberMapUpdate(pak, living); // moved here 
+
+                if (updateIcons)
+                {
+                    pak.WriteByte((byte)(0x80 | living.GroupIndex));
+                    lock (living.EffectList)
+                    {
+                        byte i = 0;
+                        foreach (IGameEffect effect in living.EffectList)
+                        {
+                            if (effect is GameSpellEffect)
+                            {
+                                i++;
+                            }
+                        }
+                        pak.WriteByte(i);
+                        foreach (IGameEffect effect in living.EffectList)
+                        {
+                            if (effect is GameSpellEffect)
+                            {
+                                pak.WriteByte(0);
+                                pak.WriteShort(effect.Icon);
+                            }
+                        }
+                    }
+                }                
+            }
+            else
+            {
+                pak.WriteInt(0x20);
+                if (updateIcons)
+                {
+                    pak.WriteByte((byte)(0x80 | living.GroupIndex));
+                    pak.WriteByte(0);
+                }
+            }
+        }
+		
         /// <summary>
         /// 1125 UDPinit reply
         /// </summary>
