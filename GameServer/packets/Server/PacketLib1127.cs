1f0423e17af0957a873ecec84e052086a875eca1
OntheDOL <OntheDOL@users.noreply.github.com>
2021-01-21 11:41:56 +0800

    fix bugged char screen 1127
    A bit of a workaround for a display bug on char screen when a player doesnt have a realm set on their account

--- GameServer/packets/Server/PacketLib1127.cs
+++ GameServer/packets/Server/PacketLib1127.cs
@@ -12,6 +12,25 @@ namespace DOL.GS.PacketHandler
         {
         }
 
+        /// <summary>
+        /// 1127 login granted packet unchanged, work around for server type
+        /// </summary>        
+        public override void SendLoginGranted(byte color)
+        {
+            // work around for character screen bugs when server type sent as 00 but player doesnt have a realm
+            // 0x07 allows for characters in all realms
+            using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.LoginGranted)))
+            {
+                pak.WritePascalString(GameClient.Account.Name);
+                pak.WritePascalString(GameServer.Instance.Configuration.ServerNameShort); //server name
+                pak.WriteByte(0x05); //Server ID, seems irrelevant
+                var type = color == 0 ? 7 : color;
+                pak.WriteByte((byte)type); // 00 normal type?, 01 mordred type, 03 gaheris type, 07 ywain type
+                pak.WriteByte(0x00); // Trial switch 0x00 - subbed, 0x01 - trial acc
+                SendTCP(pak);
+            }
+        }
+
         public override void SendMessage(string msg, eChatType type, eChatLoc loc)
         {
             if (GameClient.ClientState == GameClient.eClientState.CharScreen)
